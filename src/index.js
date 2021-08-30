11cb4e685917322052f645f7eae5656d4725c9ae
stefan <stefanace26@gmail.com>
2021-08-30 14:40:07 -0400

    workshop 2 Lazy Loading, made in course 'DOM manipulation' in Platzi

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import registerImage from './lazy'
 
 console.log('Happy hacking :)')
+
+// const url = "https://randomfox.ca/floof/"
+
+const minimum = 1
+const maximum = 122
+const random = () => Math.floor(Math.random()*(maximum - minimum)) + minimum
+
+const createImageNode = ()=>{
+	const container = document.createElement('div')
+	container.className = 'p-4'
+
+	const image = document.createElement('img')
+	image.className = 'mx-auto'
+	image.width = '320'
+	image.dataset.src = `https://randomfox.ca/images/${random()}.jpg`
+
+	container.appendChild(image)
+	return container
+}
+
+
+const mountNode = document.getElementById('images')
+
+const addButton = document.querySelector('button')
+const addImage = () => {
+	const newImage = createImageNode()
+	mountNode.appendChild(newImage)
+	registerImage(newImage)
+}
+addButton.addEventListener('click', addImage)
